afc30d9bf2c191d738e77330c19b65c0b2e59d25
Dima <skydmytro@gmail.com>
2024-10-15 13:46:25 +0200

    feat: add cors

--- index.js
+++ index.js
@@ -9,11 +9,19 @@ dotenv.config();
 const app = express();
 const port = parseInt(process.env.PORT || "3001", 10);
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://skydmytro.vercel.app/",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api", [emailRouter, downloadCvRouter]);
+app.use("/api", [emailRouter ]);
 
 app.get("/api/hello", (req, res) => {
   res.send('Hello')
